e01e288bef2746a6aff803413191a222b9988174
lamchenghou <lamchenghouu@gmail.com>
2025-02-09 01:55:50 +0800

    chore: add antd dependencies

--- apps/bug-analysis-web/next.config.mjs
+++ apps/bug-analysis-web/next.config.mjs
@@ -4,6 +4,7 @@
 const config = {
   reactStrictMode: true,
   // Transpile our internal monorepo packages on-the-fly
-  transpilePackages: ['~api'],
+  // Note next.js strict ESM resolution
+  transpilePackages: ['~api', 'rc-util', 'rc-picker'],
 };
 export default config;
