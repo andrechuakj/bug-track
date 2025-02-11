3e3492725dd69a26f398590d6315b0476f611b23
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-02-11 20:51:14 +0800

    feat: Configure API with new configurations and router

--- apps/api/src/main.py
+++ apps/api/src/main.py
@@ -1,8 +1,17 @@
+from configuration.config import load_configurations
+from configuration.logger import configure_logger
+from configuration.startup_info import configure_startup_info
+from controllers.user_controller import router as user_router
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from internal.errors import register_error_handler
 
-app = FastAPI()
-
+app = FastAPI(
+    title='bug-analysis-api',
+    lifespan=load_configurations(
+        configure_logger,
+        configure_startup_info,
+    ))
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
@@ -10,7 +19,5 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-@app.get("/")
-def test():
-    return {"message": "Hello from API!"}
+register_error_handler(app)
+app.include_router(user_router)
