1807e5951dea8712ada10b9505417d1f085a7016
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-02-14 14:32:27 +0800

    feat: Create Timestampable helper class

---
+++ apps/api/src/domain/helpers/Timestampable.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+from sqlmodel import TIMESTAMP, Column
+
+
+class Timestampable:
+    created_at: datetime | None = Column(
+        TIMESTAMP(timezone=True), default=None)
+    updated_at: datetime | None = Column(
+        TIMESTAMP(timezone=True), default=None)
+
+
+__all__ = ["Timestampable"]
