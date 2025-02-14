b4aa019df45b798422cc88792dc3a1b4cacd54d6
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-02-14 14:36:51 +0800

    feat: Implement database session management

---
+++ apps/api/src/domain/config.py
@@ -0,0 +1,30 @@
+from typing import Generator
+
+from fastapi import Depends, Request
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import Session, create_engine
+from utilities.constants import constants
+
+engine = create_engine(constants.DATABASE_URL, echo=True)
+session = sessionmaker(bind=engine, class_=Session, expire_on_commit=False,
+                       autoflush=False, autocommit=False)
+
+
+def _create_session() -> Generator[Session, None, None]:
+    with session() as s:
+        yield s
+
+
+def db_txn_manager_generator(request: Request, session: Session = Depends(_create_session)):
+    request.state.db = session
+
+
+def get_db(request: Request) -> Session:
+    return request.state.db
+
+
+__all__ = [
+    "engine",
+    "db_txn_manager_generator",
+    "get_db"
+]
--- apps/api/src/main.py
+++ apps/api/src/main.py
@@ -1,9 +1,10 @@
 from configuration.config import load_configurations
 from configuration.logger import configure_logger
 from configuration.startup_info import configure_startup_info
-from controllers.user_controller import router as user_router
 from controllers.dbms_controller import router as dbms_router
-from fastapi import FastAPI
+from controllers.user_controller import router as user_router
+from domain.config import db_txn_manager_generator
+from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from internal.errors import register_error_handler
 
@@ -12,7 +13,11 @@ app = FastAPI(
     lifespan=load_configurations(
         configure_logger,
         configure_startup_info,
-    ))
+    ),
+    dependencies=[
+        Depends(db_txn_manager_generator)
+    ],
+)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
