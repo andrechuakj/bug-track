896a520b6ca811b44c8ba60ae12255c9f80a0c06
lamchenghou <lamchenghouu@gmail.com>
2025-02-18 23:51:03 +0800

    chore: update favicon and app title

--- apps/bug-analysis-web/src/pages/_app.tsx
+++ apps/bug-analysis-web/src/pages/_app.tsx
@@ -1,16 +1,23 @@
 import type { AppProps } from 'next/app';
 
+import Head from 'next/head';
 import AppLayout from '../components/AppLayout';
 import { AppProvider } from '../utils/context';
 import './globals.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <AppProvider>
-      <AppLayout>
-        <Component {...pageProps} />
-      </AppLayout>
-    </AppProvider>
+    <>
+      <Head>
+        <title>Bug Track</title>
+        <link rel="icon" href="/favicon.ico?v=2" />
+      </Head>
+      <AppProvider>
+        <AppLayout>
+          <Component {...pageProps} />
+        </AppLayout>
+      </AppProvider>
+    </>
   );
 };
 
