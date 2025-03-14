435fe4d0e0e6751916b2b57b278c2dfa927589d9
lamchenghou <lamchenghouu@gmail.com>
2025-03-14 14:40:16 +0800

    feat: reorganise dashboard structure

--- apps/bug-analysis-web/src/api/dbms.ts
+++ apps/bug-analysis-web/src/api/dbms.ts
@@ -2,10 +2,21 @@ import { components } from '../../../api/client/api';
 
 export type DbmsResponseDto = components['schemas']['DbmsResponseDto'];
 export type BugCategory = components['schemas']['BugCategory'];
+export type AiSummary = components['schemas']['AiSummaryResponseDto'];
 
 export async function fetchDbmsData(dbms_id: number): Promise<DbmsResponseDto> {
   const res: DbmsResponseDto = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/api/v1/dbms/${dbms_id}`
+  )
+    .then((res) => res.json())
+    .catch(console.log);
+
+  return res;
+}
+
+export async function fetchAiSummary(dbms_id: number): Promise<AiSummary> {
+  const res: AiSummary = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/v1/dbms/${dbms_id}/ai_summary`
   )
     .then((res) => res.json())
     .catch(console.log);
--- apps/bug-analysis-web/src/pages/index.tsx
+++ apps/bug-analysis-web/src/pages/index.tsx
@@ -1,11 +1,18 @@
-import { Card, Col, Row, Skeleton, Typography } from 'antd';
+import { ThunderboltTwoTone } from '@ant-design/icons';
+import { Button, Card, Col, Row, Skeleton, Typography } from 'antd';
 import dynamic from 'next/dynamic';
 import { ReactNode, useEffect, useState } from 'react';
-import { BugCategory, DbmsResponseDto, fetchDbmsData } from '../api/dbms';
+import {
+  AiSummary,
+  BugCategory,
+  DbmsResponseDto,
+  fetchAiSummary,
+  fetchDbmsData,
+} from '../api/dbms';
 import CategoryTag from '../components/CategoryTag';
 import { generateBugDistrBar, generateBugTrendChart } from '../utils/chart';
 import { useAppContext } from '../utils/context';
-import { antdTagPresets } from '../utils/theme';
+import { antdTagPresets, BugTrackColors } from '../utils/theme';
 import { AppTheme } from '../utils/types';
 
 // Dynamically import ECharts for client-side rendering only
@@ -13,11 +20,24 @@ const EChartsReact = dynamic(() => import('echarts-for-react'), { ssr: false });
 
 const HomePage: React.FC = (): ReactNode => {
   const [dbmsData, setDbmsData] = useState<DbmsResponseDto>();
+  const [aiSummary, setAiSummary] = useState<AiSummary>();
+  const [aiButtonLoading, setAiButtonLoading] = useState(false);
+
+  const handleAiSummary = async () => {
+    setAiSummary(undefined);
+    setAiButtonLoading(true);
+    await fetchAiSummary(1).then((res: AiSummary) => setAiSummary(res));
+    setAiButtonLoading(false);
+  };
 
   useEffect(() => {
     fetchDbmsData(1).then((res: DbmsResponseDto) => setDbmsData(res));
+    handleAiSummary();
   }, []);
+
   const { theme } = useAppContext();
+  const isDarkMode = theme === 'dark';
+
   // Mock BugTallyInstance data
   // Temporary type
   const bugTallyInstances: { label: string; count: number }[] = [
@@ -41,7 +61,7 @@ const HomePage: React.FC = (): ReactNode => {
       {dbmsData && (
         <div className="px-4">
           <Row gutter={[16, 16]}>
-            <Col xs={24} md={12}>
+            <Col xs={24} md={14}>
               <Row>
                 <Card className="w-full h-[35vh] overflow-y-scroll">
                   <Typography.Title level={4} className="!mb-1">
@@ -59,48 +79,163 @@ const HomePage: React.FC = (): ReactNode => {
                   >
                     Issues currently tracked
                   </Typography.Title>
-                  <div className="h-[calc(35vh-150px)] overflow-y-scroll">
-                    {dbmsData.bug_categories.map(
-                      (cat: BugCategory, idx: number) =>
-                        cat.count > 0 ? (
-                          <CategoryTag
-                            key={idx}
-                            color={antdTagPresets[idx % antdTagPresets.length]}
-                            text={`${cat.name} | ${cat.count}`}
-                            className="mt-2"
-                          />
-                        ) : (
-                          <></>
-                        )
-                    )}
-                  </div>
+                  <EChartsReact
+                    option={generateBugTrendChart(bugTallyInstances)}
+                    style={{ height: 'calc(35vh - 150px)' }}
+                  />
                 </Card>
               </Row>
             </Col>
 
-            <Col xs={24} md={12}>
+            <Col xs={24} md={10}>
               <Card className="h-[35vh]">
-                <Typography.Title level={4}>Bug Tally Trend</Typography.Title>
-                <EChartsReact
-                  option={generateBugTrendChart(bugTallyInstances)}
-                  style={{ height: '30vh' }}
-                />
+                <Typography.Title level={4}>Key Issues</Typography.Title>
+                <Typography.Title level={4} className="!font-light">
+                  DBMS performance requires attention.
+                </Typography.Title>
+                <div className="flex flex-col justify-center h-[21vh]">
+                  <div className="flex flex-row overflow-scroll gap-2">
+                    <Card
+                      style={{
+                        borderRadius: '12px',
+                        backgroundColor: `${BugTrackColors.MAGENTA}40`,
+                        width: '33%',
+                        maxHeight: '19vh',
+                        overflow: 'scroll',
+                      }}
+                    >
+                      <Typography.Title level={4}>
+                        Number of Reporters
+                      </Typography.Title>
+                      <Typography.Title level={4} className="!font-light">
+                        123
+                      </Typography.Title>
+                    </Card>
+
+                    <Card
+                      style={{
+                        borderRadius: '12px',
+                        backgroundColor: `${BugTrackColors.GREEN}40`,
+                        width: '33%',
+                        maxHeight: '19vh',
+                        overflow: 'scroll',
+                      }}
+                    >
+                      <Typography.Title level={4}>
+                        Database Health
+                      </Typography.Title>
+                      <Typography.Title level={4} className="!font-light">
+                        Healthy
+                      </Typography.Title>
+                    </Card>
+
+                    <Card
+                      style={{
+                        borderRadius: '12px',
+                        backgroundColor: `${BugTrackColors.BLUE}40`,
+                        width: '33%',
+                        maxHeight: '19vh',
+                        overflow: 'scroll',
+                      }}
+                    >
+                      <Typography.Title level={4}>
+                        Report Last Updated
+                      </Typography.Title>
+                      <Typography.Title level={4} className="!font-light">
+                        7/3/25
+                      </Typography.Title>
+                    </Card>
+                  </div>
+                </div>
               </Card>
             </Col>
           </Row>
-          <Row className="mt-4">
-            <Card className="h-[40vh] w-[100%]">
-              <Typography.Title level={4}>
-                Bug Distribution (by category)
-              </Typography.Title>
-              <EChartsReact
-                option={generateBugDistrBar(
-                  dbmsData.bug_categories,
-                  'Bug Distribution (by Category)',
-                  (theme as AppTheme) ?? 'dark'
-                )}
-              />
-            </Card>
+          <Row className="mt-4" gutter={[16, 16]}>
+            <Col xs={24} md={10}>
+              <Card className="h-full">
+                <div className="h-[5vh] flex justify-between flex-wrap overflow-y-scroll">
+                  <img
+                    src={
+                      isDarkMode
+                        ? 'ai_summary_white.png'
+                        : 'ai_summary_black.png'
+                    }
+                    className="h-[5vh]"
+                  />
+                  <Button
+                    icon={
+                      <ThunderboltTwoTone
+                        twoToneColor={BugTrackColors.ORANGE}
+                      />
+                    }
+                    style={{
+                      background: `linear-gradient(135deg, ${BugTrackColors.MAGENTA}, ${BugTrackColors.ORANGE})`,
+                      border: `1px solid ${isDarkMode ? 'white' : 'black'}`,
+                    }}
+                    loading={aiButtonLoading}
+                    disabled={aiButtonLoading}
+                    onClick={handleAiSummary}
+                  >
+                    AI Summary
+                  </Button>
+                </div>
+                <Card
+                  className="m-1 p-3 h-[26vh] overflow-y-scroll"
+                  styles={{
+                    body: {
+                      padding: 0,
+                    },
+                  }}
+                >
+                  <Typography.Title
+                    level={5}
+                    className="!font-light text-justify !leading-[1.75]"
+                  >
+                    {!aiSummary && (
+                      <div className="mr-4">
+                        <Skeleton active />
+                      </div>
+                    )}
+                    {aiSummary && (
+                      <p className="fade-in-text ">{aiSummary?.summary}</p>
+                    )}
+                  </Typography.Title>
+                </Card>
+              </Card>
+            </Col>
+            <Col xs={24} md={14}>
+              <Card className="h-[40vh] w-[100%]">
+                <div className="h-[5vh]">
+                  <Typography.Title level={4}>
+                    Bug Distribution (by category)
+                  </Typography.Title>
+                </div>
+
+                <EChartsReact
+                  option={generateBugDistrBar(
+                    dbmsData.bug_categories,
+                    'Bug Distribution (by Category)',
+                    (theme as AppTheme) ?? 'dark'
+                  )}
+                  style={{ height: '20vh' }}
+                />
+                <div className="h-[calc(15vh-48px)] overflow-y-scroll p-1 flex flex-wrap">
+                  {dbmsData.bug_categories.map(
+                    (cat: BugCategory, idx: number) =>
+                      cat.count > 0 ? (
+                        <CategoryTag
+                          key={idx}
+                          color={antdTagPresets[idx % antdTagPresets.length]}
+                          text={`${cat.name} | ${cat.count}`}
+                          className="mt-2"
+                        />
+                      ) : (
+                        <></>
+                      )
+                  )}
+                </div>
+              </Card>
+            </Col>
           </Row>
           <Row gutter={[16, 16]} className="mt-4">
             <Col xs={24} md={12}>
