a8d1a737cea3337dea2283bc0881342db714a54d
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-03-17 05:18:38 +0800

    feat: Support passing additional arguments to errors

--- apps/api/src/internal/errors/client_errors.py
+++ apps/api/src/internal/errors/client_errors.py
@@ -5,40 +5,40 @@ from fastapi import HTTPException
 
 @dataclass(frozen=True)
 class BaseClientError(HTTPException):
-    def __init__(self, status_code: int, detail: str):
-        super().__init__(status_code=status_code, detail=detail)
+    def __init__(self, status_code: int, detail: str, **kwargs):
+        super().__init__(status_code=status_code, detail=detail, **kwargs)
         self.status_code = status_code
         self.detail = detail
 
 
 class BadRequestError(BaseClientError):
-    def __init__(self, detail: str):
-        super().__init__(400, detail)
+    def __init__(self, detail: str, /, **kwargs):
+        super().__init__(400, detail, **kwargs)
 
 
 class UnauthorizedError(BaseClientError):
-    def __init__(self, detail: str):
-        super().__init__(401, detail)
+    def __init__(self, detail: str, /, **kwargs):
+        super().__init__(401, detail, **kwargs)
 
 
 class ForbiddenError(BaseClientError):
-    def __init__(self, detail: str):
-        super().__init__(403, detail)
+    def __init__(self, detail: str, /, **kwargs):
+        super().__init__(403, detail, **kwargs)
 
 
 class NotFoundError(BaseClientError):
-    def __init__(self, detail: str):
-        super().__init__(404, detail)
+    def __init__(self, detail: str, /, **kwargs):
+        super().__init__(404, detail, **kwargs)
 
 
 class MethodNotAllowedError(BaseClientError):
-    def __init__(self, detail: str):
-        super().__init__(405, detail)
+    def __init__(self, detail: str, /, **kwargs):
+        super().__init__(405, detail, **kwargs)
 
 
 class UnprocessableEntityError(BaseClientError):
-    def __init__(self, detail: str):
-        super().__init__(422, detail)
+    def __init__(self, detail: str, /, **kwargs):
+        super().__init__(422, detail, **kwargs)
 
 
 __all__ = [
