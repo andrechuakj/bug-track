e105f1766be6aa8ac521323afa3cbb612b243cd5
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-03-17 01:18:30 +0800

    feat: Connect users API to controller and use DI

--- apps/api/src/controllers/user_controller.py
+++ apps/api/src/controllers/user_controller.py
@@ -1,5 +1,6 @@
 from typing import Sequence
 
+from domain.config import get_db
 from domain.models.User import User
 from domain.views.user import UserCreateRequestDto, UserSummaryResponseDto
 from fastapi import APIRouter, Request
@@ -10,26 +11,30 @@ router = APIRouter(prefix='/api/v1/users', tags=['users'])
 
 
 @router.get('/')
-async def get_all_users() -> Sequence[UserSummaryResponseDto]:
-    return UserService.get_users()
+async def get_all_users(r: Request) -> Sequence[UserSummaryResponseDto]:
+    tx = get_db(r)
+    return UserService.get_users(tx)
 
 
 @router.get('/{user_id}')
-async def get_single_user(user_id: int, request: Request) -> UserSummaryResponseDto:
-    user = UserService.get_user(user_id)
+async def get_single_user(user_id: int, r: Request) -> UserSummaryResponseDto:
+    tx = get_db(r)
+    user = UserService.get_user(tx, user_id)
     if user is None:
         raise NotFoundError('User not found')
     return user
 
 
 @router.post('/')
-async def create_user(dto: UserCreateRequestDto) -> UserSummaryResponseDto:
+async def create_user(dto: UserCreateRequestDto, r: Request) -> UserSummaryResponseDto:
+    tx = get_db(r)
     user_to_create = User(**dto.model_dump())
-    return UserService.save_user(user_to_create)
+    return UserService.save_user(tx, user_to_create)
 
 
 @router.delete('/{user_id}')
-async def delete_user(user_id: int) -> UserSummaryResponseDto:
-    return UserService.delete_user(user_id)
+async def delete_user(user_id: int, r: Request) -> UserSummaryResponseDto:
+    tx = get_db(r)
+    return UserService.delete_user(tx, user_id)
 
 __all__ = ['router']
