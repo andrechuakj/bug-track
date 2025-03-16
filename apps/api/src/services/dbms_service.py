935fe40d184d46ff4fcd3a069f9acdbea65d3a88
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-03-17 04:07:31 +0800

    feat: Integrate DB into DBMS service

--- apps/api/src/services/dbms_service.py
+++ apps/api/src/services/dbms_service.py
@@ -1,106 +1,79 @@
-from collections import defaultdict
-from domain.models.Dbms import BugReport
-from domain.models.Dbms import Dbms
-from domain.views.dbms import BugCategory
+from sqlmodel import Session
+from domain.models.BugCategory import get_bug_category_by_ids
+from domain.models.BugReport import (
+    get_bug_report_by_dbms_and_category,
+    get_bug_report_by_ids,
+    get_bug_report_ids_by_dbms_id,
+    get_bug_categories_by_dbms_id,
+)
+from domain.models.DBMSSystem import *
+import random
 
 
 class _DbmsService:
 
-    def get_dbms(self) -> list[Dbms]:
-        return self._mock_dbms
+    def get_dbms(self, tx: Session):
+        return get_dbms_systems(tx)
 
-    def get_dbms_by_id(self, dbms_id: int):
-        return next((dbms for dbms in self._mock_dbms if dbms.id == dbms_id), None)
+    def get_dbms_by_id(self, tx: Session, dbms_id: int):
+        return get_dbms_system_by_id(tx, dbms_id)
 
-    def get_dbms_bug_categories(self, dbms_id: int) -> list[BugCategory]:
-        # Count bugs per category for the given dbms_id
-        bug_counts = defaultdict(int)
-        for report in self._mock_bug_reports:
-            if report.dbms_id == dbms_id:
-                bug_counts[report.category_id] += 1
-
-        # Create BugCategory list from all mock categories with counts or default 0
-        bug_categories = [
-            BugCategory(
-                id=category["id"],
-                name=category["category_name"],
-                count=bug_counts.get(category["id"], 0),
-            )
-            for category in self._mock_categories
-        ]
-
+    def get_dbms_bug_categories(self, tx: Session, dbms_id: int):
+        category_ids = get_bug_categories_by_dbms_id(tx, dbms_id)
+        print(f"Found categories: {category_ids} for dbms_id: {dbms_id}")
+        bug_categories = get_bug_category_by_ids(tx, category_ids)
         return bug_categories
 
-    """
+    def get_random_bug_descriptions_sample(
+        self,
+        tx: Session,
+        dbms_id: int,
+        sample_percent: float,
+        random_seed: int | None = None,
+    ) -> list[str]:
+        """
         Sample a random set of bug descriptions.
 
         dbms_id: dbms from which we will sample the bug descriptions
-        sample_percent: percentage of bug descriptions to sample
-    """
-
-    # TODO: Replace each step with the appropriate ORM query
-    def get_random_bug_descriptions_sample(
-        self, dbms_id: int, sample_percent: float
-    ) -> list[str]:
-        # TODO: Establish connection to the correct DBMS-tenant table
-        # Get the bug count
-        bug_count = len(self._mock_bug_reports)
-        # Get the virtual id corresponding to each row in the database
-        # Note that we do not support deletion yet, hence bug_report_id
-        # (or its equivalent) will correspond to the physical row index
-
-        # TODO: Replace the dummy code with the commented out code when
-        #       we have populated the database
-        # sample_idx = sample(range(bug_count), int(bug_count * sample_percent))
-        sample_idx = [0, 3, 25]
-        # Retrieve the corresponding rows with an IN query
-        descriptions_sample = [
-            self._mock_bug_reports[i].description for i in sample_idx
-        ]
-
-        return descriptions_sample
-
-    """
-        Params:
-            - categories: list[str]: The categories to filter by, if empty, no filter applied
-            - start should be 0-based
-    """
+        sample_percent: proportion (number between 0-1) of bug descriptions to sample
+        """
+        bug_reports = get_bug_report_ids_by_dbms_id(tx, dbms_id)
+        print(f"Found bug reports: {bug_reports} for dbms_id: {dbms_id}")
+        if random_seed is not None:
+            random.seed(random_seed)
+        sample_ids = random.sample(bug_reports, int(len(bug_reports) * sample_percent))
+        reports = get_bug_report_by_ids(tx, sample_ids)
+        return [r.description or "" for r in reports]
 
     def bug_search(
         self,
+        tx: Session,
         dbms_id: int,
         search: str,
         start: int = 0,
         limit: int = 100,
         categories: list[int] = [],
-    ) -> list[BugReport]:
+    ):
+        """
+        Params:
+            - categories: list[str]: The categories to filter by, if empty, no filter applied
+            - start should be 0-based
+        """
         # TODO: replace with correct SQL / ORM stmt, need to access correct tenant db/table as well
         # TODO: Write it in a way that each category gets almost equal amount of bug reports
-        res = [
-            rep
-            for rep in self._mock_bug_reports
-            if search.lower() in rep.report_title.lower()
-            and (rep.category_id in categories or len(categories) == 0)
-        ]
+        raise NotImplementedError("Not implemented yet")
 
-        return res[start : start + limit]
-
-    """
+    def bug_search_category(
+        self, tx: Session, dbms_id: int, category: int, start: int, amount: int = 5
+    ):
+        """
         A specific endpoint for querying ADDITIONAL bug reports, based on the current
         size distribution.
         start should be 0-based
-    """
-
-    def bug_search_category(
-        self, dbms_id: int, category: int, start: int, amount: int = 5
-    ) -> list[BugReport]:
-        # TODO: replace with correct SQL / ORM stmt
-        category_bugs = [
-            rep for rep in self._mock_bug_reports if rep.category_id == category
-        ]
-        sliced = category_bugs[start : start + amount]
-
-        return sliced
+        """
+        category_bugs = get_bug_report_by_dbms_and_category(tx, dbms_id, category)
+        # TODO: Use limit and offset in SQL query directly
+        return list(category_bugs)[start : start + amount]
 
 
 DbmsService = _DbmsService()
