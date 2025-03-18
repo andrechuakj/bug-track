cc4b192eb5056171c2ed158d38169010ddd47472
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-03-19 02:11:49 +0800

    feat: Short-circuit on failed auth check

--- apps/bug-analysis-web/src/api/authMiddleware.ts
+++ apps/bug-analysis-web/src/api/authMiddleware.ts
@@ -4,7 +4,7 @@ import { getTokens } from '../utils/auth';
 const unsecuredEnpointsRegex = /^\/public/;
 
 const authMiddleware: Middleware = {
-  onRequest: async ({ request }) => {
+  onRequest: ({ request }) => {
     const url = new URL(request.url);
     console.debug(request.method, url.pathname);
     if (unsecuredEnpointsRegex.test(url.pathname)) {
@@ -14,7 +14,9 @@ const authMiddleware: Middleware = {
     const accessToken = getTokens()?.accessToken;
     if (!accessToken) {
       console.error('No access token found');
-      throw new Error('No access token found');
+      // Early return, skip BE call.
+      // Typecast is safe due to incorrect typings in the library
+      return new Response('Unauthorized', { status: 401 }) as any;
     }
     request.headers.set('Authorization', `Bearer ${accessToken}`);
     return request;
