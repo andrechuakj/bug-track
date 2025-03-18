f8eca71aa004c4de757942e0af493b471c544e7a
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-03-19 04:19:56 +0800

    feat: Create secured endpoints middleware

---
+++ apps/api/src/internal/middleware/secured_endpoints.py
@@ -0,0 +1,33 @@
+import re
+
+from configuration.logger import get_logger
+from fastapi import Request
+from internal.errors import create_client_error_response as error
+from internal.errors.client_errors import ForbiddenError, UnauthorizedError
+from services.auth_service import AuthService
+
+unsecured_endpoints_regex = re.compile(r"^/public/.*")
+
+
+async def secured_endpoints_middleware(request: Request, call_next):
+    logger = get_logger()
+    if re.match(unsecured_endpoints_regex, request.url.path):
+        logger.info(f"Public Endpoint: {request.url.path}")
+        return await call_next(request)
+    logger.info(f"Secured Endpoint: {request.url.path}")
+    token = request.headers.get("Authorization")
+    if not token:
+        logger.warning("No token provided")
+        return error(request, UnauthorizedError("Missing access token"))
+    if not token.startswith("Bearer "):
+        logger.warning("Invalid token format")
+        return error(request, ForbiddenError("Invalid token format"))
+    token = token.replace("Bearer ", "")
+    try:
+        if not AuthService.is_access_token(token):
+            logger.warning("Invalid token type")
+            return error(request, ForbiddenError("Invalid token"))
+    except Exception as e:
+        logger.error(f"Error validating token: {e}")
+        return error(request, ForbiddenError("Invalid token"))
+    return await call_next(request)
