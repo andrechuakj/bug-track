b70ba8a3f5ae47c9f79df3a895fc4780b4fb2b36
lamchenghou <lamchenghouu@gmail.com>
2025-03-21 04:33:45 +0800

    chore: update Timestampable mixin to use SGT and for correct MRO

--- apps/api/src/domain/helpers/Timestampable.py
+++ apps/api/src/domain/helpers/Timestampable.py
@@ -1,11 +1,14 @@
+from datetime import datetime, timezone, timedelta
+from sqlmodel import Field
 from datetime import datetime
+from typing import Annotated
 
-from sqlmodel import TIMESTAMP, Column
-
+SINGAPORE_TZ = timezone(timedelta(hours=8))
+def now_sgt():
+    return datetime.now(SINGAPORE_TZ)
 
 class Timestampable:
-    created_at: datetime | None = Column(TIMESTAMP(timezone=True), default=None)
-    updated_at: datetime | None = Column(TIMESTAMP(timezone=True), default=None)
-
+    created_at: Annotated[datetime, Field(default_factory=now_sgt)]
+    updated_at: Annotated[datetime, Field(default_factory=now_sgt)]
 
 __all__ = ["Timestampable"]
