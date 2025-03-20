9050d760f855f62b726d3f717bcdba2b16f00589
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-03-20 18:52:28 +0800

    feat: Implement auth retry logic and request ID

--- apps/bug-analysis-web/src/api/authMiddleware.ts
+++ apps/bug-analysis-web/src/api/authMiddleware.ts
@@ -1,5 +1,8 @@
 import { Middleware } from '~api';
 import { getTokens } from '../utils/auth';
+import authService from './auth';
+
+const retryCache: Record<string, boolean> = {};
 
 const unsecuredEnpointsRegex = /^\/public.*/;
 
@@ -7,6 +10,11 @@ const authMiddleware: Middleware = {
   onRequest: ({ request }) => {
     const url = new URL(request.url);
     console.debug(request.method, url.pathname);
+    if (!request.headers.has('X-Request-Id')) {
+      const requestId = Math.random().toString(36).slice(16);
+      request.headers.set('X-Request-Id', requestId);
+      retryCache[requestId] = true;
+    }
     if (unsecuredEnpointsRegex.test(url.pathname)) {
       console.log('Unsecured endpoint, not attaching token');
       return undefined;
@@ -18,10 +26,37 @@ const authMiddleware: Middleware = {
       // Typecast is safe due to incorrect typings in the library
       return new Response('Unauthorized', { status: 401 }) as any;
     }
-    request.headers.set('Authorization', `Bearer ${accessToken}`);
+    if (!request.headers.has('Authorization')) {
+      request.headers.set('Authorization', `Bearer ${accessToken}`);
+    }
     return request;
   },
-  // TODO: 401 Could be due to expired token, try to refresh
+  onResponse: async ({ request, response }) => {
+    const requestId = request.headers.get('X-Request-Id')!;
+    if (response.status !== 401) {
+      console.debug('Response status:', response.status);
+      delete retryCache[requestId];
+      return response;
+    }
+    if (!retryCache[requestId]) {
+      console.error('Already retried, not retrying again');
+      delete retryCache[requestId];
+      return response;
+    }
+    console.warn('Unauthorized, retrying with refresh token');
+    const ok = await authService.refreshToken();
+    if (!ok) {
+      console.error('Failed to refresh token');
+      return response;
+    }
+    const newRequest = request.clone();
+    newRequest.headers.set(
+      'Authorization',
+      `Bearer ${getTokens()?.accessToken}`
+    );
+    retryCache[requestId] = false;
+    return await fetch(newRequest);
+  },
 };
 
 export default authMiddleware;
