3c8d3cbef98214587939be3c3401203fb49e9eb5
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-03-21 11:38:29 +0800

    chore: Remove unneeded factory

--- apps/api/src/domain/helpers/Timestampable.py
+++ apps/api/src/domain/helpers/Timestampable.py
@@ -7,13 +7,11 @@ class Timestampable(SQLModel):
     created_at: datetime | None = Field(
         sa_type=TIMESTAMP(timezone=True),
         nullable=False,
-        default_factory=datetime.now,
         sa_column_kwargs={"server_default": func.now()},
     )
     updated_at: datetime | None = Field(
         sa_type=TIMESTAMP(timezone=True),
         nullable=False,
-        default_factory=datetime.now,
         sa_column_kwargs={"server_default": func.now(), "onupdate": func.now()},
     )
 
