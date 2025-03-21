33067ebfdac3019b13b4247d6397c3867caacde3
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-03-21 20:15:59 +0800

    feat: Create testing utility

---
+++ apps/api/src/utilities/testing.py
@@ -0,0 +1,24 @@
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import Session, SQLModel, create_engine
+
+# Use in-memory SQLite database for testing
+TEST_DATABASE_URL = "sqlite://"
+
+
+def create_mock_database():
+    """
+    Mock the database for testing, using an in-memory SQLite database.
+    """
+    engine = create_engine(TEST_DATABASE_URL, echo=True)
+    SQLModel.metadata.create_all(engine)
+    generate_session = sessionmaker(
+        bind=engine,
+        class_=Session,
+        expire_on_commit=False,
+        autoflush=False,
+        autocommit=False,
+    )
+    return generate_session
+
+
+__all__ = ["create_mock_database"]
