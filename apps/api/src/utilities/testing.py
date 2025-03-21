15e8a190742dae2e49236ba9caa8be029b6d54df
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-03-21 20:43:06 +0800

    chore: Rename method

--- apps/api/src/utilities/testing.py
+++ apps/api/src/utilities/testing.py
@@ -5,7 +5,7 @@ from sqlmodel import Session, SQLModel, create_engine
 TEST_DATABASE_URL = "sqlite://"
 
 
-def create_mock_database():
+def create_test_database():
     """
     Mock the database for testing, using an in-memory SQLite database.
     """
@@ -21,4 +21,4 @@ def create_mock_database():
     return generate_session
 
 
-__all__ = ["create_mock_database"]
+__all__ = ["create_test_database"]
