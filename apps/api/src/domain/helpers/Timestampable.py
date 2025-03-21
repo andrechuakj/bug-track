2e07c91681cceadceea13333dba871391b139a96
lamchenghou <lamchenghouu@gmail.com>
2025-03-21 11:29:24 +0800

    fix: lint

--- apps/api/src/domain/helpers/Timestampable.py
+++ apps/api/src/domain/helpers/Timestampable.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from sqlmodel import TIMESTAMP, Column
 
+
 class Timestampable:
     created_at: datetime | None = Column(TIMESTAMP(timezone=True), default=None)
     updated_at: datetime | None = Column(TIMESTAMP(timezone=True), default=None)
