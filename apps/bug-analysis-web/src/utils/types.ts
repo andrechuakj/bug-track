e287226b3331f9d935a70e11fc9332a35114c03f
lamchenghou <lamchenghouu@gmail.com>
2025-03-23 15:40:35 +0800

    chore: add mapping between categories and category_id

--- apps/bug-analysis-web/src/utils/types.ts
+++ apps/bug-analysis-web/src/utils/types.ts
@@ -1,7 +1,7 @@
 export type AppTheme = 'dark' | 'light';
 
 export enum FilterBugCategory {
-  CRASH = 'Crash',
+  CRASH = 'Crash / Segmentation Fault',
   ASSERTION_FAILURE = 'Assertion Failure',
   USABILITY = 'Usability',
   COMPATIBILITY = 'Compatibility',
@@ -10,14 +10,19 @@ export enum FilterBugCategory {
   CONSTRAINT_VIOLATION = 'Constraint Violation',
   DEADLOCK = 'Deadlock',
   DATA_CORRUPTION = 'Data Corruption',
-  SQL_SYNTAX_ERROR = 'SQL Syntax Error',
+  SQL_SYNTAX_ERROR = 'SQL Syntax / Parsing Error',
   PRIVILEGE_ESCALATION = 'Privilege Escalation',
-  MEMORY_LEAK = 'Memory Leak',
-  CONCURRENCY_ISSUE = 'Concurrency Issue',
-  TRANSACTION_ANOMALY = 'Transaction Anomaly',
+  MEMORY_LEAK = 'Memory Leak / Resource Exhaustion',
+  CONCURRENCY_ISSUE = 'Concurrency Issue (race conditions, lost updates, etc.)',
+  TRANSACTION_ANOMALY = 'Transaction Anomaly (phantom reads, dirty reads, etc.)',
   NON_ISSUES = 'Non-Issues',
   NONE_SELECTED = 'None Selected',
 }
+
+export const categoryToIdMap: { [cat in FilterBugCategory]?: number } =
+  Object.fromEntries(
+    Object.values(FilterBugCategory).map((value, index) => [value, index])
+  );
 
 export enum FilterBugPriority {
   LOW = 'Low',
