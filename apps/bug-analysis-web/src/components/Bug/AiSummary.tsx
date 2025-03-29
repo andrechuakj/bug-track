e611d7a8de1a0955c2e7598c16cf34e97725a3d7
andre <andrechua.kj@gmail.com>
2025-03-29 17:09:36 +0800

    feat: use collapse component

--- apps/bug-analysis-web/src/components/Bug/AiSummary.tsx
+++ apps/bug-analysis-web/src/components/Bug/AiSummary.tsx
@@ -1,30 +1,24 @@
-import { Skeleton } from 'antd';
+import { Collapse, Skeleton, Typography } from 'antd';
 import React from 'react';
-import { useAppContext } from '../../utils/context';
 
 const AiSummary: React.FC = () => {
-  const { theme } = useAppContext();
-
   const summary = false;
 
   return (
-    <div
-      className={`w-full rounded-lg p-4 ${theme === 'light' ? 'bg-white' : 'bg-black'}`}
-    >
-      <img
-        src={
-          theme === 'dark' ? '/ai_summary_white.png' : '/ai_summary_black.png'
-        }
-        className="h-[6vh]"
-      />
-      <div className="px-3 pb-4">
-        {summary ? (
-          <></>
-        ) : (
-          <Skeleton active paragraph={{ rows: 4 }} title={false} />
-        )}
-      </div>
-    </div>
+    <Collapse
+      expandIconPosition="end"
+      items={[
+        {
+          key: '1',
+          label: <Typography.Text>✨AI Summary</Typography.Text>,
+          children: summary ? (
+            <></>
+          ) : (
+            <Skeleton active paragraph={{ rows: 4 }} title={false} />
+          ),
+        },
+      ]}
+    />
   );
 };
 
