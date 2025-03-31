620208bcda850d8a373e0689f46e52bb1daa0de0
H1410101 <H1410101@gmail.com>
2025-03-31 16:35:02 +0800

    chore: lint

--- apps/api/src/utilities/views.py
+++ apps/api/src/utilities/views.py
@@ -1,8 +1,10 @@
 from pydantic import BaseModel, ConfigDict
 
+
 def to_camel(string: str) -> str:
-    parts = string.split('_')
+    parts = string.split("_")
     return parts[0] + "".join(word.capitalize() for word in parts[1:])
+
 
 class BaseResponseModel(BaseModel):
     model_config = ConfigDict(
