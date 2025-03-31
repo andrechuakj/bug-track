da9a9c794d3c4969fae896dda29ea3713d15cf33
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-03-31 21:55:04 +0800

    fix: Fix type errors

--- apps/bug-analysis-web/src/components/Bug/BugHeader.tsx
+++ apps/bug-analysis-web/src/components/Bug/BugHeader.tsx
@@ -5,30 +5,41 @@ import BugStatusIcon from './BugStatusIcon';
 const BugHeader: React.FC = () => {
   const { bugReport, isBugLoading } = useBugReport();
 
+  if (isBugLoading) {
+    return <Skeleton active paragraph={{ rows: 1 }} />;
+  }
+
+  if (!bugReport) {
+    return (
+      <Typography.Title level={2} className="text-red-500">
+        Error loading bug report
+      </Typography.Title>
+    );
+  }
+
   return (
-    <>
-      {isBugLoading && <Skeleton active paragraph={{ rows: 1 }} />}
-      {!isBugLoading && (
-        <div className="w-full flex gap-4">
-          <div className="w-3/4">
-            <Typography.Title level={2}>
-              {bugReport?.title
-                ? `${bugReport.title} #${bugReport.id}`
-                : 'No title found'}
-            </Typography.Title>
-            {bugReport && <BugStatusIcon isClosed={bugReport.is_closed} />}
-          </div>
-          <div className="w-1/4 flex flex-col xl:flex-row xl:justify-end gap-4">
-            <Button type="primary" href={bugReport?.url} target="_blank">
-              View on GitHub
-            </Button>
-            <Button type="default" href={bugReport?.repo_url} target="_blank">
-              View Repository
-            </Button>
-          </div>
-        </div>
-      )}
-    </>
+    <div className="w-full flex gap-4">
+      <div className="w-3/4">
+        <Typography.Title level={2}>
+          {bugReport.title
+            ? `${bugReport.title} #${bugReport.id}`
+            : 'No title found'}
+        </Typography.Title>
+        <BugStatusIcon isClosed={bugReport.is_closed} />
+      </div>
+      <div className="w-1/4 flex flex-col xl:flex-row xl:justify-end gap-4">
+        {bugReport.url && (
+          <Button type="primary" href={bugReport.url} target="_blank">
+            View on GitHub
+          </Button>
+        )}
+        {bugReport.repo_url && (
+          <Button type="default" href={bugReport.repo_url} target="_blank">
+            View Repository
+          </Button>
+        )}
+      </div>
+    </div>
   );
 };
 
