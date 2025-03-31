e4054a5ddf0918f8e5ed940b645a9e4059f635da
H1410101 <H1410101@gmail.com>
2025-03-31 15:51:59 +0800

    chore: restore camelCase for fastAPI

--- apps/api/src/utilities/views.py
+++ apps/api/src/utilities/views.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel, ConfigDict
 
+def to_camel(string: str) -> str:
+    parts = string.split('_')
+    return parts[0] + "".join(word.capitalize() for word in parts[1:])
 
 class BaseResponseModel(BaseModel):
     model_config = ConfigDict(
-        # TODO: Restore camel casing
-        # alias_generator=alias_generators.to_camel,
+        alias_generator=to_camel,
         allow_population_by_field_name=True,
     )
