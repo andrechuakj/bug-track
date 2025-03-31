2cd68eddfeb3450cbe839e459c2a656ec0a13100
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-03-31 21:57:20 +0800

    refactor: Use DI for callback memoization

--- apps/bug-analysis-web/src/components/Bug/BugDescription.tsx
+++ apps/bug-analysis-web/src/components/Bug/BugDescription.tsx
@@ -1,44 +1,44 @@
 import { Divider, Skeleton, Typography } from 'antd';
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import { BugReport } from '../../api/bug_report';
 import { useBugReport } from '../../contexts/BugReportContext';
 import { useAppContext } from '../../utils/context';
 import { formatDate } from '../../utils/dateUtils';
+
+const getDateDescription = (bugReport: BugReport | null): string => {
+  if (bugReport?.is_closed && bugReport.issue_closed_at) {
+    return `Closed at: ${formatDate(bugReport.issue_closed_at)}`;
+  } else if (bugReport?.issue_created_at) {
+    return `Created at: ${formatDate(bugReport.issue_created_at)}`;
+  } else {
+    return 'No date found';
+  }
+};
 
 const BugDescription: React.FC = () => {
   const { bugReport, isBugLoading } = useBugReport();
   const { theme } = useAppContext();
 
-  const getDateDescription = (): string => {
-    if (bugReport?.is_closed && bugReport.issue_closed_at) {
-      return `Closed at: ${formatDate(bugReport.issue_closed_at)}`;
-    } else if (bugReport?.issue_created_at) {
-      return `Created at: ${formatDate(bugReport.issue_created_at)}`;
-    } else {
-      return 'No date found';
-    }
-  };
+  if (isBugLoading) {
+    return <Skeleton active paragraph={{ rows: 1 }} />;
+  }
 
   return (
-    <>
-      {isBugLoading && <Skeleton active paragraph={{ rows: 1 }} />}
-      {!isBugLoading && (
-        <div
-          className={`w-full rounded-lg p-4 ${theme === 'light' ? 'bg-white' : 'bg-black'}`}
-        >
-          <div className="markdown overflow-y-auto">
-            <Markdown remarkPlugins={[remarkGfm]} skipHtml>
-              {bugReport?.description || 'No description found'}
-            </Markdown>
-          </div>
+    <div
+      className={`w-full rounded-lg p-4 ${theme === 'light' ? 'bg-white' : 'bg-black'}`}
+    >
+      <div className="markdown overflow-y-auto">
+        <Markdown remarkPlugins={[remarkGfm]} skipHtml>
+          {bugReport?.description || 'No description found'}
+        </Markdown>
+      </div>
 
-          <Divider className="mt-4 mb-1" />
-          <Typography.Text className="text-xs">
-            {getDateDescription()}
-          </Typography.Text>
-        </div>
-      )}
-    </>
+      <Divider className="mt-4 mb-1" />
+      <Typography.Text className="text-xs">
+        {getDateDescription(bugReport)}
+      </Typography.Text>
+    </div>
   );
 };
 
