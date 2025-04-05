a458f85bf91afe036e6c501996d7cfbac612ee71
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-04-06 00:27:53 +0800

    fix: Add GitHub URL to response DTO

--- apps/api/src/domain/views/dbms.py
+++ apps/api/src/domain/views/dbms.py
@@ -38,6 +38,7 @@ class BugReportResponseDto(BaseResponseModel):
     title: str
     description: str | None
     url: str
+    github_url: str
     issue_created_at: datetime
     issue_updated_at: datetime | None
     issue_closed_at: datetime | None
