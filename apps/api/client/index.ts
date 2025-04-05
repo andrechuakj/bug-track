fb173803665638a2745fa4f5fe24526805c0906c
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-04-05 23:22:34 +0800

    feat: Create Swagger type helpers

--- apps/api/client/index.ts
+++ apps/api/client/index.ts
@@ -1,5 +1,5 @@
 import _createClient, { type ClientOptions } from 'openapi-fetch';
-import type { paths } from './api';
+import type { components, paths } from './api';
 
 const client = _createClient<paths>();
 export type ApiClient = typeof client;
@@ -9,3 +9,11 @@ export const createClient = (options: ApiClientOptions): ApiClient =>
   _createClient(options);
 
 export { type Middleware } from 'openapi-fetch';
+
+type Schemas = components['schemas'];
+
+export type Responses<T extends keyof Schemas & `${string}ResponseDto`> =
+  Schemas[T];
+
+export type RequestBody<T extends keyof Schemas & `${string}RequestDto`> =
+  Schemas[T];
