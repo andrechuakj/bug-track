157fd37f719278d0cc0e0b4be448440b9ef5ada6
lamchenghou <lamchenghouu@gmail.com>
2025-04-05 15:28:42 +0800

    feat(fe): add ability to show/hide replies and add scroll behaviour for thread

--- apps/bug-analysis-web/src/components/Thread/index.tsx
+++ apps/bug-analysis-web/src/components/Thread/index.tsx
@@ -2,7 +2,7 @@ import { ReloadOutlined } from '@ant-design/icons';
 import { Card, Divider, Input, message, Modal, Skeleton } from 'antd';
 import clsx from 'clsx';
 import { useRouter } from 'next/router';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import {
   addDiscussionReply,
   DiscussionReplyResponseDto,
@@ -14,22 +14,31 @@ import Comment from '../Comment';
 
 interface ThreadProps {
   discussion: DiscussionResponseDto;
+  isLast: boolean;
 }
 
-const Thread: React.FC<ThreadProps> = ({ discussion: ogDiscussion }) => {
+const Thread: React.FC<ThreadProps> = ({
+  discussion: ogDiscussion,
+  isLast,
+}) => {
   // Fetch thread discussion
   const [discussion, setDiscussion] =
     useState<DiscussionResponseDto>(ogDiscussion);
   const [isLoading, setIsLoading] = useState(false);
+
   // Add reply modal
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [newComment, setNewComment] = useState('');
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isRepliesVisible, setIsRepliesVisible] = useState(true);
   const { loggedInUser } = useAuth();
   const router = useRouter();
 
-  // Replies
+  // To locate the newly added reply
+  const newReplyRef = useRef<HTMLDivElement | null>(null);
+
   const handleAddCommentClick = () => {
+    setIsRepliesVisible(true);
     setIsModalVisible(true);
   };
 
@@ -54,20 +63,34 @@ const Thread: React.FC<ThreadProps> = ({ discussion: ogDiscussion }) => {
     }
 
     if (!userId) {
-      messageApi.error('Please login and try again');
+      message.error('Please login and try again');
       setIsSubmitting(false);
       setIsModalVisible(false);
       return;
     }
 
     addDiscussionReply(discussion.id, newComment, userId)
-      .then((newDiscussion) => {
+      .then((newReply) => {
         setDiscussion((prev) => ({
           ...prev,
-          replies: [...prev.replies, newDiscussion],
+          replies: [...prev.replies, newReply],
         }));
         setNewComment('');
         setIsModalVisible(false);
+
+        setTimeout(() => {
+          if (newReplyRef.current) {
+            newReplyRef.current.classList.add('highlight');
+            setTimeout(() => {
+              newReplyRef.current?.classList.remove('highlight');
+            }, 4000);
+
+            newReplyRef.current.scrollIntoView({
+              behavior: 'smooth',
+              block: 'nearest',
+            });
+          }
+        }, 100);
       })
       .catch((error) => {
         console.error('Error adding comment:', error);
@@ -82,21 +105,30 @@ const Thread: React.FC<ThreadProps> = ({ discussion: ogDiscussion }) => {
     setNewComment('');
   };
 
-  // Render info
   const refreshThread = () => {
     setIsLoading(true);
-    fetchDiscussion(discussion.id).then((data) => {
-      setDiscussion(data);
-      setIsLoading(false);
-    });
+    fetchDiscussion(discussion.id)
+      .then((data) => {
+        setDiscussion(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error refreshing thread:', error);
+        setIsLoading(false);
+      });
   };
 
   const renderComments = (
     replies: DiscussionReplyResponseDto[],
     level: number = 0
   ) => {
-    return replies.map((reply) => (
-      <div key={reply.id} style={{ marginLeft: level * 20 }} className="mb-5">
+    return replies.map((reply, index) => (
+      <div
+        key={reply.id}
+        ref={index === replies.length - 1 ? newReplyRef : null}
+        style={{ marginLeft: level * 20 }}
+        className="mb-5"
+      >
         <Comment
           idx={reply.id}
           content={reply.content}
@@ -107,57 +139,72 @@ const Thread: React.FC<ThreadProps> = ({ discussion: ogDiscussion }) => {
     ));
   };
 
-  // Toast
+  const toggleRepliesVisibility = () => {
+    setIsRepliesVisible((prev) => !prev);
+  };
+
   const [messageApi, contextHolder] = message.useMessage();
 
   return (
-    <Card className="mb-3">
+    <Card className={!isLast ? 'mb-3' : ''}>
       {contextHolder}
       {isLoading && <Skeleton active />}
       {!isLoading && (
         <>
-          {
-            <Comment
-              idx={discussion.id}
-              content={discussion.content}
-              name={discussion.author?.name}
-              timestamp={discussion.created_at}
-            />
-          }
-          {discussion.replies.length > 0 && <Divider />}
-          {renderComments(discussion.replies, 1)}
-          {
-            <div className="flex justify-between mt-4">
-              <span
-                className={clsx(
-                  'text-blue-500 cursor-pointer flex gap-2',
-                  discussion.replies.length > 0 ? 'ml-5' : ''
-                )}
-                onClick={handleAddCommentClick}
+          <Comment
+            idx={discussion.id}
+            content={discussion.content}
+            name={discussion.author?.name}
+            timestamp={discussion.created_at}
+          />
+          {discussion.replies.length > 0 && (
+            <>
+              <Divider />
+              <div
+                className="cursor-pointer text-blue-500 mb-3"
+                onClick={toggleRepliesVisibility}
               >
-                Add a reply
-              </span>
-              <ReloadOutlined onClick={refreshThread} />
-            </div>
-          }
-          {
-            <Modal
-              title="Add a reply"
-              visible={isModalVisible}
-              onOk={handleModalOk}
-              onCancel={handleModalCancel}
-              confirmLoading={isSubmitting}
-              okText="Add Reply"
-              cancelText="Cancel"
+                {isRepliesVisible ? 'Hide Replies' : 'Show Replies'}
+              </div>
+              <div
+                style={{
+                  maxHeight: isRepliesVisible ? '1000px' : '0',
+                  overflow: 'scroll',
+                  transition: 'max-height 0.3s ease',
+                }}
+              >
+                {renderComments(discussion.replies, 1)}
+              </div>
+            </>
+          )}
+          <div className="flex justify-between mt-4">
+            <span
+              className={clsx(
+                'text-blue-500 cursor-pointer flex gap-2',
+                discussion.replies.length > 0 && isRepliesVisible ? 'ml-5' : ''
+              )}
+              onClick={handleAddCommentClick}
             >
-              <Input.TextArea
-                rows={4}
-                value={newComment}
-                onChange={(e) => setNewComment(e.target.value)}
-                placeholder="Write your comment here..."
-              />
-            </Modal>
-          }
+              Add a reply
+            </span>
+            <ReloadOutlined onClick={refreshThread} />
+          </div>
+          <Modal
+            title="Add a reply"
+            visible={isModalVisible}
+            onOk={handleModalOk}
+            onCancel={handleModalCancel}
+            confirmLoading={isSubmitting}
+            okText="Add Reply"
+            cancelText="Cancel"
+          >
+            <Input.TextArea
+              rows={4}
+              value={newComment}
+              onChange={(e) => setNewComment(e.target.value)}
+              placeholder="Write your comment here..."
+            />
+          </Modal>
         </>
       )}
     </Card>
