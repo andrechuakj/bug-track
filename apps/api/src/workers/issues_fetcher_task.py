8d7b3eb9435947e988aa0899449d6a066545b1e5
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-04-13 04:24:43 +0800

    chore: Use enum value instead of string

--- apps/api/src/workers/issues_fetcher_task.py
+++ apps/api/src/workers/issues_fetcher_task.py
@@ -3,6 +3,7 @@ from datetime import datetime, timezone
 
 from configuration.logger import get_logger
 from domain.config import get_session
+from domain.enums import PriorityLevel
 from domain.models.BugReport import BugReport
 from domain.models.DBMSSystem import get_dbms_systems
 from github import (
@@ -112,7 +113,7 @@ def fetch_github_issues_task(self):
                                     else None
                                 ),
                                 is_closed=issue.state == "closed",
-                                priority="Unassigned",
+                                priority=PriorityLevel.Unassigned,
                             )
                             BugReport.save_bug_report(session, bug_report)
                             total_issues_count += 1
