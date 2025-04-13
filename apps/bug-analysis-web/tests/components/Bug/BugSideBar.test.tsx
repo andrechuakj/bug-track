0f0fe51a9d6b780c87eab3399f324f52374be3d4
H1410101 <H1410101@gmail.com>
2025-04-13 21:35:40 +0800

    tests: BugSideBar

--- apps/bug-analysis-web/tests/components/Bug/BugSideBar.test.tsx
+++ apps/bug-analysis-web/tests/components/Bug/BugSideBar.test.tsx
@@ -1,4 +1,10 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import {
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+  within,
+} from '@testing-library/react';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { BugCategoryResponseDto, BugReport } from '../../../src/api/bugReport';
 import BugSideBar from '../../../src/components/Bug/BugSideBar';
@@ -6,21 +12,25 @@ import {
   mockBugReportData,
   MockBugReportProvider,
 } from '../../contexts/MockBugReportProvider';
+import { BugPriority } from '../../../src/utils/types';
 
-const { mockFetchAllCategories, mockUpdateBugCategory } = vi.hoisted(() => ({
-  mockFetchAllCategories: vi.fn(),
-  mockUpdateBugCategory: vi.fn(),
-}));
+const { mockFetchAllCategories, mockUpdateBugCategory, mockUpdateBugPriority } =
+  vi.hoisted(() => ({
+    mockFetchAllCategories: vi.fn(),
+    mockUpdateBugCategory: vi.fn(),
+    mockUpdateBugPriority: vi.fn(),
+  }));
 
 vi.mock('../../../src/api/bugReport', () => ({
   fetchAllCategories: mockFetchAllCategories,
   updateBugCategory: mockUpdateBugCategory,
+  updateBugPriority: mockUpdateBugPriority,
 }));
 
 const mockCategories: BugCategoryResponseDto[] = [
-  { id: 1, name: 'UI', count: 5 },
-  { id: 2, name: 'Backend', count: 10 },
-  { id: 3, name: 'Database', count: 3 },
+  { id: 1, name: 'Category A', count: 5 },
+  { id: 2, name: 'Category B', count: 10 },
+  { id: 3, name: 'Category C', count: 6 },
 ];
 mockFetchAllCategories.mockResolvedValue(mockCategories);
 
@@ -68,6 +78,127 @@ describe('BugSideBar', () => {
     await waitFor(() => {
       expect(screen.getByText(mockBugReportData.category)).toBeInTheDocument();
       expect(screen.getByText(mockBugReportData.dbms)).toBeInTheDocument();
+    });
+  });
+
+  it('renders category menu based on fetched categories', async () => {
+    const { getByText, findByRole } = renderComponent({
+      isBugLoading: false,
+      bugReport: mockBugReportData,
+    });
+
+    await waitFor(() => expect(mockFetchAllCategories).toHaveBeenCalled());
+
+    const categoryEditIcon =
+      getByText('Category').parentElement?.querySelector('a');
+    if (categoryEditIcon) {
+      fireEvent.click(categoryEditIcon);
+    }
+
+    const dropdownMenu = await findByRole('menu');
+    expect(dropdownMenu).toBeInTheDocument();
+
+    await waitFor(async () => {
+      for (const category of mockCategories) {
+        expect(
+          await within(dropdownMenu).findByText(category.name)
+        ).toBeInTheDocument();
+      }
+    });
+  });
+
+  it('updates categories', async () => {
+    const newCategory = mockCategories[1];
+    const updatedBugReport = {
+      ...mockBugReportData,
+      category_id: newCategory.id,
+      category: newCategory.name,
+    };
+    mockUpdateBugCategory.mockResolvedValue(updatedBugReport);
+
+    const { findByRole, getByRole } = renderComponent({
+      isBugLoading: false,
+      bugReport: mockBugReportData,
+    });
+
+    await waitFor(() => expect(mockFetchAllCategories).toHaveBeenCalled());
+
+    const categoryEditIcon = screen
+      .getByText('Category')
+      .parentElement?.querySelector('a');
+    if (categoryEditIcon) {
+      fireEvent.click(categoryEditIcon);
+    }
+
+    const dropdownMenu = await findByRole('menu');
+    expect(dropdownMenu).toBeInTheDocument();
+    const menuItem = await within(dropdownMenu).findByText(newCategory.name);
+    fireEvent.click(menuItem);
+
+    expect(getByRole('img', { name: /loading/i })).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(mockUpdateBugCategory).toHaveBeenCalledWith(
+        mockBugReportData.id,
+        newCategory.id
+      );
+    });
+  });
+
+  it('renders priority menu based on fetched priorities', async () => {
+    renderComponent({ isBugLoading: false, bugReport: mockBugReportData });
+
+    const priorityEditIcon = screen
+      .getByText('Bug Priority')
+      .parentElement?.querySelector('a');
+    if (priorityEditIcon) {
+      fireEvent.click(priorityEditIcon);
+    }
+
+    const dropdownMenu = await screen.findByRole('menu');
+    expect(dropdownMenu).toBeInTheDocument();
+
+    await waitFor(async () => {
+      for (const priorityValue of Object.values(BugPriority)) {
+        expect(
+          await within(dropdownMenu).findByText(priorityValue)
+        ).toBeInTheDocument();
+      }
+    });
+  });
+
+  it('updates priority', async () => {
+    const newPriority = BugPriority.HIGH;
+    const updatedBugReport = {
+      ...mockBugReportData,
+      priority: newPriority,
+    };
+    mockUpdateBugPriority.mockResolvedValue(updatedBugReport);
+
+    const { findByRole, getByRole } = renderComponent({
+      isBugLoading: false,
+      bugReport: mockBugReportData,
+    });
+
+    const priorityEditIcon = screen
+      .getByText('Bug Priority')
+      .parentElement?.querySelector('a');
+    if (priorityEditIcon) {
+      fireEvent.click(priorityEditIcon);
+    }
+
+    const dropdownMenu = await findByRole('menu');
+    expect(dropdownMenu).toBeInTheDocument();
+    const menuItem = await within(dropdownMenu).findByText(newPriority);
+    fireEvent.click(menuItem);
+
+    expect(getByRole('img', { name: /loading/i })).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(mockUpdateBugPriority).toHaveBeenCalledWith(
+        mockBugReportData.id,
+        newPriority
+      );
     });
   });
 });
